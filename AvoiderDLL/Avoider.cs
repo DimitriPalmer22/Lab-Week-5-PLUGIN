9b4c68f71ff6338e4bdefcfa529f5cd70c2d60e6
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-10-04 19:25:06 -0400

    Update Avoider.cs

--- AvoiderDLL/Avoider.cs
+++ AvoiderDLL/Avoider.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace AvoiderDLL;
 
-public class Avoider
+public class Avoider : MonoBehaviour
 {
+    private void Update()
+    {
+        TestUpdate();
+    }
+
+    private void TestUpdate()
+    {
+        Debug.Log("The DLL Works!");
+    }
 }
